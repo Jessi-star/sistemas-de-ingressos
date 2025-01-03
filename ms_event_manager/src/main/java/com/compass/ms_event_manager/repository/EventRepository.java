5c53fe371609e75fdee97dc8e441f60e3bad612d
Jessi-star <jessicarodrigues.enf1@gmail.com>
2025-01-03 12:04:03 -0300

    fix/corrigindo duplicidade

--- ms_event_manager/src/main/java/com/compass/ms_event_manager/repository/EventRepository.java
+++
@@ -1,7 +0,0 @@
-package com.compass.ms_event_manager.repository;
-
-import com.compass.ms_event_manager.model.Event;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface EventRepository extends MongoRepository<Event, String> {
-}
